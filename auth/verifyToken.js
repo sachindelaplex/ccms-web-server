0fbbb4ba9d0492c5b7d7ba11943e405f948c4030
Pankaj Thakre <sachin.ramteke@delaplex.in>
2019-08-21 16:58:20 +0530

    Added verify token

--- auth/verifyToken.js
+++ auth/verifyToken.js
@@ -1,12 +1,15 @@
-
 module.exports = function verifyToken(req, res, next) {
-    const bearerHeader = req.headers["authorization"];
-    if (typeof bearerHeader !== "undefined") {
-      const bearer = bearerHeader.split(" ");
-      const bearerToken = bearer[1];
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    if (bearerToken !== "null") {
       req.token = bearerToken;
       next();
     } else {
       res.sendStatus(403);
     }
+  } else {
+    res.sendStatus(403);
   }
+};
